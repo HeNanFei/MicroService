17ef754c0f2a8ae7473785ba8c4b0670faf0f1e3
zjt <872060821@qq.com>
2019-12-11 18:48:29 +0800

    自定义负载均衡

--- port80/src/main/java/com/zjt/server/Port.java
+++ port80/src/main/java/com/zjt/server/Port.java
@@ -1,11 +1,14 @@
 package com.zjt.server;
 
+import com.zjt.config.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
 @EnableEurekaClient
 @SpringBootApplication
-
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MySelfRule.class)
 public class Port {
     public static void main(String[] args) {
         SpringApplication.run(Port.class,args);
