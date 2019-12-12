1b7eccbc95f6198ed4f9d9484fce75eeceb51fbc
zjt <872060821@qq.com>
2019-12-12 11:26:54 +0800

    Feign接口调用

---
+++ publicapi/src/main/java/com/zjt/pojo/service/DeptClientService.java
@@ -0,0 +1,26 @@
+package com.zjt.pojo.service;
+
+import com.zjt.pojo.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService
+{
+    @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") long id);
+
+    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
+    public List<Dept> list();
+
+    @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
+    public boolean add(Dept dept);
+}
+
+
